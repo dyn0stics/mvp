2887fb4d91e270a2abe5793f08ecc1657c7514a6
Ivan Reif <ivanreif@gmail.com>
2018-07-11 15:34:43 +0200

    profile progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
+import 'typeface-roboto';
 import logo from './Dyno_Logo_small.png';
 import './App.css';
-import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+import { Grid, Row } from 'react-flexbox-grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 class App extends Component {
 
@@ -12,7 +19,8 @@ class App extends Component {
         this.state = {
             profile: {
                 username: ""
-            }
+            },
+            registerDialogOpen: false
         };
     }
 
@@ -34,10 +42,25 @@ class App extends Component {
     }
 
     createAccount(){
+        const apiUrl = window.API_URL;
+        let self = this;
+        axios.get(apiUrl + "user/register?username=" + self.state.profile.username)
+        .then(result => {
+            console.log(result.data);
+        })
+    }
+
+    handleOpen(){
+        this.setState({registerDialogOpen: true});
+    }
+
+    handleClose(){
+        this.setState({registerDialogOpen: false});
     }
 
     render() {
         return (
+          <Grid fluid>
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
@@ -45,27 +68,67 @@ class App extends Component {
                 <p className="App-intro">
                     {this.state.nodeInfo}
                 </p>
-                <Grid container spacing={24}>
-                    <Grid item xs={12}>
-                        <TextField
-                            id="username"
-                            label="Username"
-                            value={this.state.profile.username}
-                            onChange={this.handleUsernameChange.bind(this)}
-                            margin="normal"
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={this.createAccount.bind(this)}
-                        >
-                            Register
-                        </Button>
-                    </Grid>
-                </Grid>
+             <Dialog
+                open={this.state.registerDialogOpen}
+                onClose={this.handleClose.bind(this)}
+                aria-labelledby="form-dialog-title"
+              >
+                <DialogTitle id="form-dialog-title">Register</DialogTitle>
+
+                <DialogContent>
+                  <DialogContentText>
+                    Please complete the form with initial information related to your profile.
+                  </DialogContentText>
+                  <Grid>
+                    <Row>
+                      <TextField
+                          id="username"
+                          label="Username"
+                          value={this.state.profile.username}
+                          onChange={this.handleUsernameChange.bind(this)}
+                          margin="normal"
+                      />
+                    </Row>
+                    <Row>
+                      <TextField
+                          id="age"
+                          label="Age"
+                          value={this.state.profile.username}
+                          onChange={this.handleUsernameChange.bind(this)}
+                          margin="normal"
+                      />
+                    </Row>
+                    <Row>
+                      <TextField
+                          id="weight"
+                          label="Weight"
+                          value={this.state.profile.username}
+                          onChange={this.handleUsernameChange.bind(this)}
+                          margin="normal"
+                      />
+                    </Row>
+                      </Grid>
+                </DialogContent>
+
+                <DialogActions>
+                  <Button onClick={this.handleClose.bind(this)} color="primary">
+                    Cancel
+                  </Button>
+                  <Button onClick={this.createAccount.bind(this)} color="primary">
+                    Complete
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                onClick={this.handleOpen.bind(this)}
+            >
+                Register
+            </Button>
             </div>
+            </Grid>
         )
             ;
     }
